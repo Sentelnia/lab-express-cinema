eb6388b91813a55c062fb33a5eafb5b91be0c6d7
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-05-05 13:29:01 +0200

    suite

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,9 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+/* GET movies page */ 
+router.get('/movies', (req, res, next) =>{
+    res.render('movies')
+})
+
 module.exports = router;
