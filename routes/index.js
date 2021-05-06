2f83271baa9036a74bd8b136fd6456bce8077463
Michel Hardy <michel.robin.hardy@gmail.com>
2021-05-06 19:52:27 +0200

    Ite3

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model.js")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
-/* GET movies page */ 
-router.get('/movies', (req, res, next) =>{
-    res.render('movies')
+/* GET movies page */
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then(allMovieFromDb => {
+            console.log(allMovieFromDb);
+            //res.render('movies', { movies: allMovieFromDb });
+        })
+        .catch(error => {
+            console.log(error);
+        });
 })
 
 module.exports = router;
